36abc12fd3464c690984575aa0fe432e22d26ca0
Bipin joshi <141641444+bipingithubtech@users.noreply.github.com>
2025-01-29 19:17:09 +0530

    Update database.js

--- server/utility/database.js
+++ server/utility/database.js
@@ -8,7 +8,7 @@ const DatabaseCofig = async () => {
   try {
     mongoose.set("debug", true);
     await mongoose.connect(
-      "mongodb+srv://bipincloudshope12:EDDL3X2cP6XlSVJV@cluster0.pyhii.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+     process.env.db
     );
     console.log("Database connected successfully");
   } catch (error) {
