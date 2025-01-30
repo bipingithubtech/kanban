e8f11c1cd19db47f0c323ea09a2bccc7b4c285c3
bipinjoshi <bipinjoshicr712@gmail.com>
2025-01-30 16:25:20 +0530

    last change

--- server/utility/database.js
+++ server/utility/database.js
@@ -7,13 +7,11 @@ const DatabaseCofig = async () => {
 
   try {
     mongoose.set("debug", true);
-<<<<<<< HEAD
+
     await mongoose.connect(process.env.db);
-=======
-    await mongoose.connect(
-     process.env.db
-    );
->>>>>>> 42c151edb1e007536563460d9426c2d92e369a2a
+
+    await mongoose.connect(process.env.db);
+
     console.log("Database connected successfully");
   } catch (error) {
     console.log("Unable to set the database :=>", error);
